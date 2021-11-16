dd86a7af82e0407da3392f9a077482d7c5cf72fc
Monicag17 <93927406+Monicag17@users.noreply.github.com>
2021-11-16 12:00:14 -0500

    Add files via upload

---
+++ phyton-first-proyect-main/area_circle/main.py
@@ -0,0 +1,15 @@
+import math
+print("Area de un circulo")
+
+
+radio = 2.5
+print(f"Su radio es de: {radio}")
+
+
+pi = math.pi
+radio_cuadrado = pow(radio,2)
+
+area = pi*radio_cuadrado
+
+print(f"El area del círculo es: {area}")
+
---
+++ phyton-first-proyect-main/main.py
@@ -0,0 +1,17 @@
+# phyton-first-proyect
+
+string_var1 = "Bienvenidos"
+
+
+interger_var1 = 123
+interger_var2 = 255
+
+float_var1 = 123.24
+float_var2 = 830.7
+
+suma_entero = interger_var1+interger_var2
+suma_decimal = float_var1+float_var2
+
+print (suma_entero)
+print (suma_decimal)
+print (string_var1 + " a su clase de programación")
