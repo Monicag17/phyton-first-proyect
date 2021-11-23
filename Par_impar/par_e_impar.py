d4c4dfba731595b64c79c5acc59c601126010c01
Monica Gutierrez <monica.gutierrez01@uptc.edu.co>
2021-11-22 19:53:59 -0500

    Definicion par e impar

---
+++ Par_impar/par_e_impar.py
@@ -0,0 +1,10 @@
+print("Programa para determinar si un número es par o impar")
+
+numero = int(input("Digite el numero que desea: "))
+print (f"El numero es {numero}")
+
+if numero%2==0:
+    print("El número que digito es par")
+else:
+    print("El numero que digito es impar")
+
