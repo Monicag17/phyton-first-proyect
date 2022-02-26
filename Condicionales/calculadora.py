a8d3d845fd2fd83753c7262861eaed5d8b50d42d
Monica Gutierrez <monica.gutierrez01@uptc.edu.co>
2022-02-25 20:23:55 -0500

    Ciclo while calculadora

--- Condicionales/calculadora.py
+++ Condicionales/calculadora.py
@@ -11,6 +11,10 @@ def menu_opciones():
     print('1. Suma    2. Resta    3. Multiplicación   4. División 5. Finalizar programa  ')
     return int(input())
 
+def menufinalizar_programa():
+    print('Desea usted continuar realizando operaciones')
+    print(' 1. Si     2. No')
+    return int(input())
 
 def numero_operacion():
     print('Por favor digite el número para la operación: ')
@@ -42,25 +46,46 @@ def finalizar_programa():
 
 
 mensaje_bienvenida()
+
 operacion = menu_opciones()
-numero1 = numero_operacion()
-numero2 = numero_operacion()
 
-if operacion == 1:
-    suma_numeros(numero1, numero2)
+while operacion<5:
+    numero1 = numero_operacion()
+    numero2 = numero_operacion()
 
+    if operacion == 1:
+        suma_numeros(numero1, numero2)
 
-elif operacion == 2:
-    resta_numeros(numero1, numero2)
+    elif operacion == 2:
+        resta_numeros(numero1, numero2)
 
-elif operacion == 3:
-    multi_numeros(numero1, numero2)
+    elif operacion == 3:
+        multi_numeros(numero1, numero2)
 
-elif operacion == 4:
-    if numero2 == 0:
-        divi0_numeros(numero2)
+    elif operacion == 4:
+        if numero2 == 0:
+            divi0_numeros(numero2)
+        else:
+            divi_numeros(numero1, numero2)
+
     else:
-        divi_numeros(numero1, numero2)
+        finalizar_programa()
 
-else:
-    finalizar_programa()
+    finalizar = menufinalizar_programa()
+
+    if finalizar == 1:
+        operacion = menu_opciones()
+
+    else:
+        print('Programa finalizado gracias por utilizar la calculadora')
+        break
+
+
+
+
+
+
+
+
+
+
